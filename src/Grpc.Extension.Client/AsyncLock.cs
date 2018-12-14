c3b7046f283d21b6b5b7df9949e293f8830ea2fe
wangzelin <315872858@qq.com>
2018-12-14 18:33:19 +0800

    init commit

---
+++ src/Grpc.Extension.Client/AsyncLock.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Grpc.Extension.Client
+{
+	public class AsyncLock
+	{
+		private readonly SemaphoreSlim _semaphoreSlim;
+
+		public AsyncLock()
+		{
+			_semaphoreSlim = new SemaphoreSlim(0, 1);
+		}
+
+		public async Task<Release> GetLockAsync(int expire)
+		{
+			await _semaphoreSlim.WaitAsync(expire);
+			return new Release(_semaphoreSlim);
+		}
+
+		public void ReleaseLock()
+		{
+			_semaphoreSlim.Release(1);
+		}
+	}
+
+	public struct Release : IDisposable
+	{
+		private readonly SemaphoreSlim _semaphoreSlim;
+
+		public Release(SemaphoreSlim semaphoreSlim)
+		{
+			_semaphoreSlim = semaphoreSlim;
+		}
+
+		public void Dispose()
+		{
+			_semaphoreSlim.Release();
+		}
+	}
+}
--- src/Grpc.Extension.Client/ChannelFactory.cs
+++ src/Grpc.Extension.Client/ChannelFactory.cs
@@ -10,29 +10,31 @@ namespace Grpc.Extension.Client
 {
 	public class ChannelFactory
 	{
-		private ConcurrentDictionary<string, List<ChannelEndPoint>> EndPoint { get; }
+		private Dictionary<string, List<ChannelEndPoint>> ServiceEndPoints { get; }
 
 		private GrpcClientConfiguration GrpcClientConfiguration { get; }
 
 		private object LockObject { get; set; }
+		private static AsyncLock asyncLock = new AsyncLock();
 
 		public ChannelFactory(GrpcClientConfiguration _grpcClientConfiguration)
 		{
-			EndPoint = new ConcurrentDictionary<string, List<ChannelEndPoint>>();
+			ServiceEndPoints = new Dictionary<string, List<ChannelEndPoint>>();
 			GrpcClientConfiguration = _grpcClientConfiguration;
 		}
 
-		public async Task RefurbishChannel()
-		{
 
-			lock (LockObject)
+
+		public async Task RefreshChannels()
+		{
+			using (asyncLock.GetLockAsync(1000 * 10))
 			{
 				foreach (var serviceName in GrpcClientConfiguration.GrpcServiceName)
 				{
-					if (!EndPoint.ContainsKey(serviceName))
+					var entries = await GetServiceEndPoints(serviceName);
+					if (!ServiceEndPoints.ContainsKey(serviceName))
 					{
-						var entries = GetService(serviceName).GetAwaiter().GetResult();
-						EndPoint.TryAdd(serviceName, entries.Select(p => new ChannelEndPoint
+						ServiceEndPoints.Add(serviceName, entries.Select(p => new ChannelEndPoint
 						{
 							Address = p.Service.Address,
 							Port = p.Service.Port,
@@ -42,14 +44,41 @@ namespace Grpc.Extension.Client
 						continue;
 					}
 
+					if (ServiceEndPoints.TryGetValue(serviceName, out var endPoints))
+					{
+						var newEndPoints = entries.FindAll(p => !endPoints.Any(e => e.Address == p.Service.Address && e.Port == p.Service.Port));
+						if (newEndPoints.Any())
+						{
+							endPoints.AddRange(newEndPoints.Select(p => new ChannelEndPoint
+							{
+								Address = p.Service.Address,
+								Port = p.Service.Port,
+								Channel = new Channel(p.Service.Address, p.Service.Port, ChannelCredentials.Insecure),
+								Status = ChannelEndPointStatus.Passing
+							}));
+						}
 
+						foreach (var entry in newEndPoints)
+						{
+							var endPoint = endPoints.FirstOrDefault(p => p.Address == entry.Service.Address && p.Port == entry.Service.Port);
+							if (endPoint == null)
+								endPoints.Add(new ChannelEndPoint
+								{
+									Address = entry.Service.Address,
+									Port = entry.Service.Port,
+									Channel = new Channel(entry.Service.Address, entry.Service.Port, ChannelCredentials.Insecure),
+									Status = ChannelEndPointStatus.Passing
+								});
+							continue;
 
+						}
+					}
 				}
 			}
 		}
 
 
-		public async Task<List<ServiceEntry>> GetService(string serviceId, string tag = "", bool passingOnly = true)
+		public async Task<List<ServiceEntry>> GetServiceEndPoints(string serviceId, string tag = "", bool passingOnly = true)
 		{
 			using (var consul = new ConsulClient(config => { config.Address = new Uri("http://192.168.1.142:8500"); }))
 			{
